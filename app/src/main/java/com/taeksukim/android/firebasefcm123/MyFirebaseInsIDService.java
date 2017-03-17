c92ec03ea0d8988bb743acfb9448cf45e49ec67d
TaeksuKim <taeka89@gmail.com>
2017-03-17 10:06:22 +0900

    FireBase notification

---
+++ app/src/main/java/com/taeksukim/android/firebasefcm123/MyFirebaseInsIDService.java
@@ -0,0 +1,21 @@
+package com.taeksukim.android.firebasefcm123;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+/*
+
+ */
+
+
+public class MyFirebaseInsIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("IDService", "Refreshed token: " + refreshedToken);
+        // 서버를 구현한후 refreshedToken 을 서버측에 전달해줘야 한다
+    }
+}
